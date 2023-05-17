d1ffd956b5e5521ca76b63fb6b5b2e83f4dd9196
kimbro97 <rlagudwog@gmail.com>
2023-05-17 19:52:09 +0900

    상품 등록 폼

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -32,6 +33,16 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(Model model) {
+        return "basic/addForm";
     }
 
     @PostConstruct
